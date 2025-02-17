3b84a97e77bd097647db542fd1a7b599ff3e0f6b
H3nry-T <ht3drex@gmail.com>
2025-02-17 19:57:35 +0000

    userRepository with new queries and service with new functions

--- repositories/UserRepository.py
+++ repositories/UserRepository.py
@@ -6,3 +6,19 @@ class UserRepository:
     
     def get_user_by_username(self, username): 
         return User.query.filter_by(username=username).first()
+
+    def find_by_username(self, username):
+        # Implement database query
+        return self.db.execute('SELECT * FROM users WHERE username = ?', (username,)).fetchone()
+
+    def find_by_email(self, email):
+        # Implement database query
+        return self.db.execute('SELECT * FROM users WHERE email = ?', (email,)).fetchone()
+
+    def create_user(self, user):
+        # Implement user creation
+        self.db.execute(
+            'INSERT INTO users (username, email, password) VALUES (?, ?, ?)',
+            (user['username'], user['email'], user['password'])
+        )
+        self.db.commit()
--- services/UserService.py
+++ services/UserService.py
@@ -9,6 +9,14 @@ class AuthenticationService:
             return False, "Username already exists"
         user = self.user_repository.create_user(username, password)
         return True, "User registered successfully"
+
+    # Helps check if the username already exists before registering
+    def validate_username(self, username):
+        username_exists =  self.user_repository.get_user_by_username(username)
+        if username_exists:
+            # do not allow username to be used
+            return False
+        return True
     
     def authenticate(self, username: str, password: str) -> bool: 
         user = self.user_repository.get_user_by_username(username)
