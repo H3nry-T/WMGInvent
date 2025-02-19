3e318bb0a228666eedaf669adc807c1f84ff3b8e
H3nry-T <ht3drex@gmail.com>
2025-02-19 02:18:03 +0000

    typed my service and repository

--- repositories/UserRepository.py
+++ repositories/UserRepository.py
@@ -4,13 +4,10 @@ class UserRepository:
     def __init__(self, db): 
         self.db = db 
     
-    def get_user_by_username(self, username): 
+    def get_user_by_username(self, username: str) -> User | None: 
         return User.query.filter_by(username=username).first()
 
-    def find_by_email(self, email):
-        return User.query.filter_by(email=email).first()
-
-    def create_user(self, username: str, hashed_password: str):
+    def create_user(self, username: str, hashed_password: str) -> User:
         user = User()
         user.username = username
         user.password_hash = hashed_password
--- services/UserService.py
+++ services/UserService.py
@@ -1,12 +1,14 @@
 from werkzeug.security import check_password_hash, generate_password_hash
 from repositories.UserRepository import UserRepository
+from models.UserModel import User
 from typing import Dict
 
 class AuthenticationService: 
     def __init__(self, user_repository: UserRepository): 
         self.user_repository = user_repository
     
-    def register_user(self, username: str, password: str): 
+    # Registers a new user and returns the user object if successful or raises an exception if not
+    def register_user(self, username: str, password: str) -> User: 
         if self.username_available(username):
             hashed_password = generate_password_hash(password)
             user = self.user_repository.create_user(username, hashed_password)
@@ -15,14 +17,15 @@ class AuthenticationService:
             raise Exception("problem with registering user")
 
     # Helps check if the username already exists before registering
-    def username_available(self, username):
+    def username_available(self, username: str) -> bool:
         username_exists =  self.user_repository.get_user_by_username(username)
         if username_exists:
             # do not allow username to be used
             return False
         return True
     
-    def authenticate_user(self, username: str, password: str) -> bool: 
+    # Authenticates the user and returns the user object if successful or raises an exception if not
+    def authenticate_user(self, username: str, password: str) -> User: 
         user = self.user_repository.get_user_by_username(username)
         if user and check_password_hash(user.password_hash, password):
             return user
