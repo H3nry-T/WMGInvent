6ac99457ccf052b46fd8d1b82d0be6fee39885d0
H3nry-T <ht3drex@gmail.com>
2025-02-20 11:40:50 +0000

    Create ElectronicsService with repository interaction methods

---
+++ services/ElectronicsService.py
@@ -0,0 +1,29 @@
+from repositories.IRepository import IRepository
+from models.ElectronicModel import Electronic
+from typing import List
+
+class ElectronicsService: 
+    def __init__(self, inventory_repository: IRepository):
+        self.inventory_repository = inventory_repository
+
+    def get_all_electronics(self) -> List[Electronic]:
+        return self.inventory_repository.get_all()
+    
+    def get_electronics_by_id(self, id):
+        return self.inventory_repository.get_by_id(id)
+    
+    def create_electronic(self, electronic): 
+        return self.inventory_repository.create(electronic)
+    
+    def count_electronics(self): 
+        return self.inventory_repository.count()
+    
+    def get_paginated_electronics(self, page, per_page): 
+        return self.inventory_repository.get_paginated(page, per_page)
+    
+    
+    
+    
+    
+    
+    
