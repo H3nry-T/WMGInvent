1347ff475cbe162182c65d9b7522a09d24494fb1
H3nry-T <ht3drex@gmail.com>
2025-02-20 11:33:19 +0000

    Add json method to Electronic model for serialization

--- models/ElectronicModel.py
+++ models/ElectronicModel.py
@@ -1,7 +1,7 @@
 from models.ProductModel import Product
 from models.Category import ProductCategory
 from global_db_object import db
-
+from typing import Optional
 class Electronic(Product):
     __tablename__ = 'electronics'
     id = db.Column(db.Integer, db.ForeignKey('products.id'), primary_key=True)
@@ -21,5 +21,29 @@ class Electronic(Product):
         'polymorphic_identity': ProductCategory.ELECTRONICS,
     }
 
+    #for debugging prints
     def __repr__(self):
         return f'<Electronic {self.name}>'
+    
+    def json(self): 
+        """transforms the ElectronicModel into a json format"""
+        return {
+            'id': self.id,
+            'name': self.name, 
+            'description' : self.description,
+            'specification': self.specification,
+            'category' : self.category,
+            'price' : self.price,
+            'stock': self.stock,
+            'power_rating': self.power_rating,
+            'battery_life': self.battery_life,
+            'energy_efficiency_rating': self.energy_efficiency_rating,
+            'manufacturer': self.manufacturer,
+            'warranty_period': self.warranty_period,
+            'dimensions': self.dimensions,
+            'weight': self.weight,
+            'operating_system': self.operating_system,
+            'processor': self.processor,
+            'ram': self.ram,
+            'storage': self.storage,
+        }
